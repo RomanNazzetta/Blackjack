e1db4538467aba399ed87a1a338cdae2481d3f4e
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-15 11:54:13 -0300

    text.contenct

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 let firstCard = 12
-let secondCard = 19
+let secondCard = 9
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
 
 function startGame (){
     if (sum <= 20) {
-        message = "Do you want to draw a new card?🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Whooo! You've got Blackjack!🥳"
+        message = "You've got Blackjack!"
         hasBlackjack = true
     } else {
-        message = "You're out of the game!😭"
+        message = "You're out of the game!"
         isAlive = false
     }    
-    //Log it out
-    console.log(message)
+    messageEl.textContent = message
 }
 
 
