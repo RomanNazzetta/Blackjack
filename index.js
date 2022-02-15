7d2e05535e459195445b8c47b5e264005f1d6c39
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-15 10:29:29 -0300

    less equ

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let firstCard = 5
+let firstCard = 20
 let secondCard = 8
 let sum = firstCard + secondCard
 
-if (sum < 21){
-    console.log("Do you want to draw a new card?")
-} else if (sum === 21){
-    console.log("Whooo! You've got Blackjack!")
-} else if (sum > 21){
-    console.log("You're out of the game!")
+if (sum < 21) {
+    console.log("Do you want to draw a new card?🙂")
+} else if (sum === 21) {
+    console.log("Whooo! You've got Blackjack!🥳")
+} else {
+    console.log("You're out of the game!😭")
 }
---
+++ test.js
@@ -0,0 +1,7 @@
+let age = 22
+
+if (age < 21){
+    console.log("You can not enter the club!")
+} else {
+    console.log("Welcome!")
+}
