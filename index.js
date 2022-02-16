0077015604f53470f34d695a6c2abc91a0957946
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-16 10:30:46 -0300

    rename start game

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-
 function startGame (){
+    rendertGame()
+}
+
+function rendertGame (){
     cardsEl.textContent = "Cards: " + firstCard + " - " + secondCard
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
@@ -29,7 +32,7 @@ function newCard(){
 
     let card = 5
     sum += card
-    startGame()
+    renderGame()
 }
 
 
