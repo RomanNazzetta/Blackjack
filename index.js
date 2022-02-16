87edd6e69cbc453a1704701143174891f1495d7f
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-16 10:10:07 -0300

    cardsEl

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ let hasBlackjack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-//let sumEl = document.getElementById("sum-el")
-let sumEl = document.querySelector(".sum-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
 
 function startGame (){
+    cardsEl.textContent = "Cards: " + firstCard + " - " + secondCard
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
