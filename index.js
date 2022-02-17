19568b4b1c6e870aff508d0a9d8c9e05974579d5
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-17 11:35:26 -0300

    startGame function fixed

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-
+console.log(cards)
 
 function getRandomCard(){
     let randomNumber = Math.floor (Math.random () *13 ) +1
@@ -22,6 +20,11 @@ function getRandomCard(){
 }
 
 function startGame (){
+    let isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard +" "+ secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
