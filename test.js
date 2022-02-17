68df4295220ca186073e7862f7b7d727eaf03005
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-17 10:16:11 -0300

    return

--- test.js
+++ test.js
@@ -39,3 +39,25 @@
 // for (let i = 0; i< cartas.length; i += 1){
 //     console.log(cartas[i])
 // }
+
+let player1Time = 102
+let player2Time = 107
+
+// cmd+d - ctrl+d
+function getFastestRaceTime() {
+    if (player1Time < player2Time) {
+        return player1Time
+    } else if (player2Time < player1Time) {
+        return player2Time
+    } else {
+        return player1Time
+    }
+}
+
+function totalRaceTime(){
+    return player1Time + player2Time
+}
+
+let totalTime = totalRaceTime()
+
+console.log(totalTime)
